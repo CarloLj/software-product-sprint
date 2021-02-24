12cf628cf4ae776914c53a3345d71b294b3c05fc
Carlo Lujan <carlouxljlj@gmail.com>
2021-02-24 06:31:55 +0000

    Navbar+animations+css+wireframes+aboutme in progress

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Bienvenido a mi portafolio', 'Welcome to my portfolio', 'Bienvenue dans mon portfolio'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
